95959c7ae2887e94b8796c221cd6f166ee59a6cd
Elmir Bikchurin <uuum1984@mail.ru>
2025-12-10 03:44:35 +0300

    first

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,20 @@ import vue from '@vuejs/plugin-vue'
 
 export default defineConfig({
     plugins: [vue()],
-    base: '/weather/', // Должно быть именно так
+    base: '/weather/', // Убедитесь, что это именно так
     build: {
         outDir: 'dist',
         assetsDir: 'assets',
-        emptyOutDir: true
+        emptyOutDir: true,
+        rollupOptions: {
+            output: {
+                assetFileNames: (assetInfo) => {
+                    if (assetInfo.name === 'index.css') return 'assets/style.css'
+                    return 'assets/[name]-[hash][extname]'
+                },
+                chunkFileNames: 'assets/[name]-[hash].js',
+                entryFileNames: 'assets/[name]-[hash].js'
+            }
+        }
     }
 })
